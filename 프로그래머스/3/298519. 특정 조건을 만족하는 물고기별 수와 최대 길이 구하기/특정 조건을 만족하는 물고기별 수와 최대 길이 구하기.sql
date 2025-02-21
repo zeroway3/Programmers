ead4cf1dc4324cfcb506a35b8316a1713a75d73a
youngsik <165105348+zeroway3@users.noreply.github.com>
2025-02-21 09:59:04 +0900

    Update 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,4 +1,3 @@
--- 코드를 작성해주세요
 SELECT COUNT(FISH_TYPE) AS FISH_COUNT
         ,MAX(LENGTH) AS MAX_LENGTH
         ,FISH_TYPE
@@ -8,3 +7,5 @@ GROUP BY FISH_TYPE
 HAVING AVG(IFNULL(LENGTH,'10'))>=33
 
 ORDER BY FISH_TYPE ASC
+
+다른 조건들은 쉽게 구했지만 GROUP BY HAVING 절에 IFNULL조건 넣는 것을 모르고 헤맨 문제
