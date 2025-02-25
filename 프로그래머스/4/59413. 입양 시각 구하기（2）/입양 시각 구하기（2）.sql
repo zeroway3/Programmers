f81b21fd8aaf2b583c57df7c9ef3c3369c6e7b54
youngsik <165105348+zeroway3@users.noreply.github.com>
2025-02-25 16:59:48 +0900

    Update 입양 시각 구하기（2）.sql

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,9 +1,9 @@
 WITH RECURSIVE hours AS (
-    SELECT 0 AS HOUR
-    UNION ALL
-    SELECT HOUR + 1
+    SELECT 0 AS HOUR -- 재귀 초기값
+    UNION ALL -- 반드시 UNION 함수 사용
+    SELECT HOUR + 1 -- 재귀
     FROM hours
-    WHERE HOUR < 23
+    WHERE HOUR < 23 --- 재귀 정지 조건
 )
 
 SELECT h.HOUR
@@ -13,3 +13,15 @@ LEFT JOIN ANIMAL_OUTS a
 ON h.HOUR = HOUR(a.DATETIME)
 GROUP BY h.HOUR
 ORDER BY h.HOUR;
+
+** 기본 재귀-쿼리
+    
+with recursive rc as ( -- RECURSIVE 재귀-쿼리문
+select 1 as h -- 재귀 초기값
+union all -- -- 반드시 UNION 함수 사용
+select h + 1 -- 재귀
+from rc 
+where h < 5 -- 재귀 정지 조건
+)
+
+select * from rc
