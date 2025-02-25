980429dae4f9efe487a93d179718830182fe8a18
youngsik <165105348+zeroway3@users.noreply.github.com>
2025-02-25 13:35:47 +0900

    Update 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -18,3 +18,30 @@ ON E.EMP_NO = G.EMP_NO
 
 GROUP BY E.EMP_NO, E.EMP_NAME, E.SAL
 ORDER BY E.EMP_NO;
+
+**최적화 코드(서브쿼리)
+
+SELECT E.EMP_NO,
+       E.EMP_NAME,
+       G.GRADE,
+       CASE G.GRADE
+           WHEN 'S' THEN ROUND(E.SAL * 0.2)
+           WHEN 'A' THEN ROUND(E.SAL * 0.15)
+           WHEN 'B' THEN ROUND(E.SAL * 0.1)
+           ELSE 0
+       END AS BONUS
+FROM HR_EMPLOYEES E
+JOIN (
+    SELECT EMP_NO,
+           CASE 
+               WHEN AVG(SCORE) >= 96 THEN 'S'                                                                                                                                                                                                                                                                                
+               WHEN AVG(SCORE) >= 90 THEN 'A'
+               WHEN AVG(SCORE) >= 80 THEN 'B'
+               ELSE 'C'
+           END AS GRADE
+    FROM HR_GRADE
+    WHERE YEAR = 2022
+    GROUP BY EMP_NO
+) G
+ON E.EMP_NO = G.EMP_NO
+ORDER BY E.EMP_NO;
